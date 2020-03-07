6c3c89b7405926f842639eafc806a84ae4d0eb5d
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2020-03-07 15:19:59 -0800

    Refactor & Commenting
    Added comments
    Updated Window title

--- Window.java
+++ Window.java
@@ -1,5 +1,5 @@
 /**
- *
+ * A modified JFrame class that will serve as the main window of the file manager.
  * @author Bradley Nickle
  */
 import javax.swing.*;
@@ -8,9 +8,14 @@ import java.awt.event.*;
 
 /**
  * Our Window class.
- *
  */
 public class Window extends JFrame{
+    // Version number
+    final private double version = 0.1;
+    /***
+     * Start a window.
+     * @param args Required Java syntax for main()
+     */
     public static void main(String[] args){
         Window w = new Window();
     }
@@ -19,22 +24,23 @@ public class Window extends JFrame{
      * Default Window constructor.
      */
     public Window(){
+        // Size the Window
         setBounds(100,100,640,480);
+        // Place the Window on the screen
         setLocationRelativeTo(null);
-        setTitle("Hello world!");
+        // Name the Window
+        setTitle("File Manager " + version);
         
-        String[] data = {"test zero",
-                         "test one",
-                         "test two",
-                         "test three",
-                         "test four",
-                         "test five",
-                         "test six",
-                         "test seven",
-                         "test eight",
-                         "test nine"};
-        DirectoryPanel dp = new DirectoryPanel(data);
-        setContentPane(new FileManagerUI(dp,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
+        // Set up test data NOTE this will eventually be replaced with actual file data
+        String[] data = {"test zero","test one",
+                         "test two","test three",
+                         "test four","test five",
+                         "test six","test seven",
+                         "test eight","test nine"};
+        
+        // Create a directory panel
+        DirectoryPanel dp = new DirectoryPanel("",data);
+        setContentPane(new FileManagerUI(dp));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
