9fe0f01e7bc7ed7d18765184d6291d584361219e
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2020-03-07 15:19:11 -0800

    Comments & Updated Components
    Added Comments
    Added configureFile() method to get data about a File whenever that data might need to be updated
    Added methods to support selection
    FilePanels will be highlighted in blue when clicked, indicating selection

--- FilePanel.java
+++ FilePanel.java
@@ -1,58 +1,153 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
 
 /**
  *
  * @author Bradley Nickle
  */
 public class FilePanel extends JPanel implements MouseListener{
-    private JTextField filename;
-    private JLabel size,dateCreated,dateModified;
+    // Fields that will represent the file. Declared & instantiated in order from left to right.
+    private JLabel pic,filename,size,dateCreated,dateModified;
+    // An image that will be tied to this.pic.
+    private ImageIcon icon;
+    // Tracks whether or not this file should be selected.
+    private boolean isSelected;
     
+    /**
+     * Default constructor for FilePanels.
+     * @param fn 
+     */
     public FilePanel(String fn){
         setLayout(new FlowLayout(FlowLayout.LEFT));
-        filename = new JTextField(fn);
-        filename.setEditable(false);
+        
+        /* Configure the components for all fields relevant to the file represented
+        by this FilePanel */
+        
+        // Configure the icon
+        icon = new ImageIcon("src/main/java/icons/file.png",fn);
+        pic = new JLabel(icon);
+        pic.addMouseListener(this);
+        add(pic);
+        
+        // Configure name
+        filename = new JLabel(fn);
         filename.addMouseListener(this);
+        filename.setToolTipText(fn);
         add(filename);
+        
+        // Configure size
         size = new JLabel("0");
+        size.setToolTipText("Size in bytes");
         size.addMouseListener(this);
         add(size);
+        
+        // Configure date created
         dateCreated = new JLabel("1/1/1970 00:00");
         dateCreated.addMouseListener(this);
+        dateCreated.setToolTipText("Date Created");
         add(dateCreated);
+        
+        // Configure date modified
         dateModified = new JLabel("1/1/1970 00:00");
         dateModified.addMouseListener(this);
+        dateModified.setToolTipText("Date Modified");
         add(dateModified);
+        
+        // Configure the file, filling in the fields with appropriate names and dates
+        configureFile();
+        
+        // Add a MouseListener & deselect this
         addMouseListener(this);
+        select(false);
+    }
+    
+    private void configureFile(){
+        File self = new File(filename.getText());
+        size.setText(Long.toString(self.length()));
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
+        //dateCreated.setText(sdf.format(self.))
+        try{
+            dateModified.setText(sdf.format(self.lastModified()));
+        }
+        catch (SecurityException e){
+            System.out.println(e.getMessage());
+            dateModified.setText("01/01/1970 00:00");
+        }
+        
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getFileName(){
+        return filename.getText();
+    }
+    
+    /**
+     * 
+     * @param b 
+     */
+    public void select(boolean b){
+        isSelected = b;
+        if (isSelected){
+            setBackground(Color.blue);
+        } else{
+            setBackground(Color.white);
+        }
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean isSelected(){
+        return isSelected;
     }
 
+    /**
+     * Overridden MouseListener method.
+     * @param e the MouseEvent to be processed.
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (!filename.isEditable()){
-            filename.setEditable(true);
-        }
-        else{
-            filename.setEditable(false);
-        }
+        select(!isSelected);
     }
 
+    /**
+     * Overridden MouseListener method.
+     * @param e the MouseEvent to be processed.
+     */
     @Override
     public void mousePressed(MouseEvent e) {
         
     }
 
+    /**
+     * Overridden MouseListener method.
+     * @param e the MouseEvent to be processed.
+     */
     @Override
     public void mouseReleased(MouseEvent e) {
         
     }
 
+    /**
+     * Overridden MouseListener method.
+     * @param e the MouseEvent to be processed.
+     */
     @Override
     public void mouseEntered(MouseEvent e) {
         
     }
 
+    /**
+     * Overridden MouseListener method.
+     * @param e the MouseEvent to be processed.
+     */
     @Override
     public void mouseExited(MouseEvent e) {
         
