39f96e86563bc296b6d7cc61f954f036089c3db8
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2020-03-28 14:02:05 -0700

    Observes Navigators
    Renamed navigation to  navButtons for clarity
    Updated constructor
    Now tracks the current directory via its observed Navigator
    Added update() to update itself on state changes from Navigator

--- FileManagerToolbar.java
+++ FileManagerToolbar.java
@@ -6,34 +6,44 @@ import javax.swing.*;
  * A modified JPanel that will act as our horizontal toolbar.
  * @author Bradley Nickle
  */
-public class FileManagerToolbar extends JPanel implements ActionListener{
-   private ButtonGroup navigation;
+public class FileManagerToolbar extends JPanel implements ActionListener,NavigatorObserver{
+   /* GUI components; back/forward buttons, address/search bars. */
+   private ButtonGroup navButtons;
    private JButton backButton,forwardButton;
    private JTextField addressBar,searchBar;
+   /* The Navigator */
+   private Navigator nav;
+   private String current;
 
    /**
     * Default FileManagerToolbar constructor.
     */
-   public FileManagerToolbar(){
+   public FileManagerToolbar(Navigator n){
+       // Observe the Navigator
+       nav = n;
+       nav.attach(this);
+       // Get the current directory
+       current = nav.getDirectory();
+       
        // Configure layout manager
        setLayout(new FlowLayout(FlowLayout.LEFT));
 
        // Instantiate a buttongroup for back/forward buttons
-       navigation = new ButtonGroup();
+       navButtons = new ButtonGroup();
 
        // Configure buttons
        backButton = new JButton("<-"); // Instantiate the back button
        backButton.setToolTipText("Back"); // add a tooltip
        // add an action listener to it; it won't trigger any code if we don't do this
        backButton.addActionListener(this);
-       navigation.add(backButton); // add it to the button group
+       navButtons.add(backButton); // add it to the button group
        add(backButton); // add it to the toolbar
 
        // Repeat all back button steps for the forward button
        forwardButton = new JButton("->");
        forwardButton.setToolTipText("Forward");
        forwardButton.addActionListener(this);
-       navigation.add(forwardButton);
+       navButtons.add(forwardButton);
        add(forwardButton);
 
        // Configure address bar
@@ -65,10 +75,31 @@ public class FileManagerToolbar extends JPanel implements ActionListener{
        if (e.getSource() == myComponent_n){...;} */
 
        if (e.getSource() == backButton){
+           // TODO tell the Navigator to go back
            System.out.println("Back not yet supported");
+           System.out.println(addressBar.getText());
        }
        if (e.getSource() == forwardButton){
+           // TODO tell the Navigator to go forward
            System.out.println("Forward not yet supported");
        }
+       if (e.getSource() == addressBar){
+           // TODO determine wether or not enter was pressed, then 
+       }
    }
+
+    /**
+     * Overridden NavigatorObserver method.
+     * Updates the FileManagerToolbar based on changes in the Navigator, mostly
+     * the changes to the Navigator's current directory.
+     * @param s the Subject to be updated
+     */
+    @Override
+    public void update(Subject s) {
+        /* this only needs to be updated if current doesn't match nav's current
+        directory */
+        if (!current.equals(nav.getDirectory())){
+            // TODO
+        }
+    }
 }
