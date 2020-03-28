eb4dbb7ee26c1d6ec83cf4dd888f2a8aa7e2f59c
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2020-03-28 13:58:31 -0700

    Updated constructor
    Renamed dp to centerPanel
    Moved initialization of FileManagerToolbar to this function
    Added initialization of a Navigator, nav

--- Window.java
+++ Window.java
@@ -26,17 +26,26 @@ public class Window extends JFrame{
     public Window(){
         // Size the Window
         setBounds(100,100,640,480);
+        
         // Place the Window on the screen
         setLocationRelativeTo(null);
+        
         // Name the Window
         setTitle("File Manager " + version);
         
         // Set up test data NOTE this will eventually be replaced with actual file data
         String path = "C:\\Users\\aloaf\\Pictures";
         
-        // Create a directory panel
-        DirectoryPanel dp = new DirectoryPanel(path);
-        setContentPane(new FileManagerUI(dp));
+        // Initialize the navigator
+        Navigator nav = new Navigator(path);
+        
+        // Initialize the GUI components
+        DirectoryPanel centerPanel = new DirectoryPanel(nav);
+        FileManagerToolbar upperToolbar = new FileManagerToolbar(nav);
+        FileManagerUI ui = new FileManagerUI(centerPanel,upperToolbar);
+        
+        // Set up the GUI
+        setContentPane(ui);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
