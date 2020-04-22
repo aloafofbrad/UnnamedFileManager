ad43cdce76e7641e0f03f054f06825d0ab6dae97
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2020-04-21 21:15:28 -0700

    Implemented code to reset scrollbar positions on update() calls

--- FileManagerUI.java
+++ FileManagerUI.java
@@ -5,9 +5,10 @@ import javax.swing.*;
  * A slightly modified JScrollPane class that will act as the main part of the UI.
  * @author Bradley Nickle
  */
-public class FileManagerUI extends JScrollPane{
+public class FileManagerUI extends JScrollPane implements NavigatorObserver{
     // Vertical and horizontal scrollbar visibility policies
     private int vsb,hsb;
+    private Navigator nav;
 
     /**
      * Default FileManagerUI constructor.
@@ -15,8 +16,9 @@ public class FileManagerUI extends JScrollPane{
      *     Intended to be a DirectoryPanel (see DirectoryPanel.java)
      * @param colHeader the Component to be viewed as the upper toolbar
      *     Intended to be a FileManagerToolbar
+     * @param name
      */
-    public FileManagerUI(Component view,Component colHeader, Component name){
+    public FileManagerUI(Navigator n,Component view,Component colHeader,Component name) {
         // Call super()
         super(view,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         
@@ -29,10 +31,29 @@ public class FileManagerUI extends JScrollPane{
         // We don't really need to store the FileManagerToolbar as a member, since getColumnHeader() isn't being overridden
         setColumnHeaderView(colHeader);
 
-        //setColumnHeaderView(name);
+        nav = n;
+        nav.attach(this);
         
         // Increase the distance scrolled by per scroll; the default is somewhat slow.
         verticalScrollBar.setUnitIncrement(16);
         horizontalScrollBar.setUnitIncrement(16);
+    }
+    
+    /**
+     * Overridden NavigatorObserver method.
+     * Sends the scrollbars back to their minimum positions when a directory is
+     * loaded or updated. This will usually happen when the user visits a new
+     * directory.
+     * 
+     * The vertical scrollbar will be set as high as possible.
+     * The horizontal scrollbar will be set as far left as possible.
+     * @param s the subject triggering the update
+     */
+    @Override
+    public void update(Subject s){
+        int minimum = verticalScrollBar.getMinimum();
+        verticalScrollBar.setValue(minimum);
+        minimum = horizontalScrollBar.getMinimum();
+        horizontalScrollBar.setValue(minimum);
     }
 }
